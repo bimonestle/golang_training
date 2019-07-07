08c48c2b1ba31f117dd872f25e71a599b962fba0
bimonestle <bimonestle@yahoo.co.id>
2019-07-07 20:13:15 +0700

    Learning Pointers

---
+++ 08.pointers/02.dereferencing/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 43
+	fmt.Println(a)  // 43
+	fmt.Println(&a) // 0x...
+
+	var b = &a
+	fmt.Println(b)  // 0x...
+	fmt.Println(*b) // 43. This is dereferencing
+
+	// b is a int pointer
+	// b points to the memory address where an int is stored
+	// to see the value in that memory address, add an * in front of b
+	// this is known as dereferencing
+	// the * is an operator in this case
+}
---
+++ 08.pointers/03.using-pointers/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 43
+
+	fmt.Println(a)  // 43
+	fmt.Println(&a) // 0x...
+
+	var b = &a
+	fmt.Println(b)  // 0x...
+	fmt.Println(*b) // 43
+
+	*b = 42        // b says, "The value at this address, changed to 42"
+	fmt.Println(a) // 42
+
+	// this is useful
+	// we can pass a memory address instead of a bunch of values (we can pass a reference)
+	// and then we can still change the value of whatever is stored at the memory address
+	// this makes our program more performant
+}
